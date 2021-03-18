792731f0b58f42c88fddeecb432ea3f5e6afb9ca
Nikith <Nikith@edureka>
2021-03-18 12:13:20 +0530

    Added A route

--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Home Route');
+})
 app.listen(3000, () => {
     console.log(`Listening on port 3000`);
 })
